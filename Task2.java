5b1dd8c18152b4c5593eccd781c6ad9edd3ddc9d
Swapnil-ai01 <swapnil011199@gmail.com>
2025-03-31 19:15:40 +0530

    Delete Task2.java

--- Task2.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class Task2 {
-    public static void main(String[] args){
-        Scanner myobj = new Scanner(System.in);
-        System.out.print("Enter string:");
-        String userName = myobj.nextLine();
-        System.out.println("You entered:"+" "+ userName);
-    }
-    }
