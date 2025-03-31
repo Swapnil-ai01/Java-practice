1fa016802a2da5bf0222074d4edf788c082d679e
Swapnil-ai01 <swapnil011199@gmail.com>
2025-03-31 19:28:10 +0530

    Add files via upload

---
+++ Task09.java
@@ -0,0 +1,16 @@
+class Task9{
+    public static void main(String[] args){
+        int intNum = 100;
+        double doubleNum = intNum;
+        System.out.println("Implicit typecasting");
+        System.out.println(" Integer value : " + intNum);
+        System.out.println(" Convert to double : " + doubleNum);
+
+        double newDouble = 99.99;
+        int newInt = (int) newDouble;
+        System.out.println("Explicit typecasting");
+        System.out.println(" Double value : " + newDouble);
+        System.out.println(" Convert to int : " + newInt);
+
+    }
+}
