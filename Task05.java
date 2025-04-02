ba9bcdc7f2218ba239da70c87183ebfbdaa64b98
Swapnil-ai01 <swapnil011199@gmail.com>
2025-04-02 19:04:51 +0530

    Updated

--- Task05.java
+++ Task05.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
-class Task5{
+class Task05{
     public static void main(String[] args){
       Scanner num = new Scanner(System.in);
        System.out.print("Write any number to find Factorial : ");
         int a =  num.nextInt();
+        num.close();
         int fact = 1;
         if (a == 0){
             fact = 1;
