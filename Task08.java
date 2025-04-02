39613937aaa2aacb4d94ace50d7828ad85f4b2d8
Swapnil-ai01 <swapnil011199@gmail.com>
2025-04-02 19:12:08 +0530

    Add files via upload

---
+++ Task08.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Task08{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number for reverse the digits: ");
+        int OrgNum = sc.nextInt();
+        int  rev=0;
+        while(OrgNum != 0){
+            rev = rev * 10 + (OrgNum % 10);
+            OrgNum /= 10;
+        }
+        System.out.println("The reverse numberis : "+rev);
+
+    }
+}
