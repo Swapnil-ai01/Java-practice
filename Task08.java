d758469a002d75e62bd1a6473da0d654126ffdeb
Swapnil-ai01 <swapnil011199@gmail.com>
2025-04-02 19:12:57 +0530

    Updated

--- Task08.java
+++ Task08.java
@@ -4,6 +4,7 @@ class Task08{
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter any number for reverse the digits: ");
         int OrgNum = sc.nextInt();
+        sc.close();
         int  rev=0;
         while(OrgNum != 0){
             rev = rev * 10 + (OrgNum % 10);
