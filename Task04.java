390b23c723ad33a8dcab4c652981249026f2ce67
Swapnil-ai01 <swapnil011199@gmail.com>
2025-04-02 19:07:45 +0530

    Updated

--- Task04.java
+++ Task04.java
@@ -4,7 +4,7 @@ class Task04{
     Scanner num = new Scanner(System.in);
     System.out.print("Write any number: ");
     int a = num.nextInt();
-    num.close()
+    num.close();
     int sum = 0;
     for(int i=0 ; i<=a ; i++){
         sum += i;
