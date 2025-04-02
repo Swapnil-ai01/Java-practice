f2aa1e0bf44ff691497041f486cb5e15af721502
Swapnil-ai01 <swapnil011199@gmail.com>
2025-04-02 19:06:03 +0530

    Updated

--- Task06.java
+++ Task06.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
-class Task6{
+class Task06{
     public static void main(String[] args){
         int rem;
         int sum = 0;
         Scanner num = new Scanner(System.in);
         System.out.print("Write a number for sum of the digits : ");
         int a = num.nextInt();
+        num.close();
         while(a != 0){
             rem = a % 10;
             sum = sum + rem;
