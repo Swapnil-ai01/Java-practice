1d68e1f454b33412a27bfe00d4ca8ac9a02b1f72
Swapnil-ai01 <swapnil011199@gmail.com>
2025-04-02 19:00:31 +0530

    Updated

--- Task02.java
+++ Task02.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
-class Task2 {
+class Task02 {
     public static void main(String[] args){
-        Scanner myobj = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter string:");
-        String userName = myobj.nextLine();
+        String userName = sc.nextLine();
         System.out.println("You entered:"+" "+ userName);
+        sc.close();
     }
     }
