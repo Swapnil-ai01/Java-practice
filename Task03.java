3b9f9f55b3426bfbbe18bec487a744aa93ed35f5
Swapnil <swapnil011199@gmail.com>
2025-04-06 19:37:17 +0530

    Updated

--- Task03.java
+++ Task03.java
@@ -1,17 +1,28 @@
-import java.util.Scanner;
+//Please give two number and after that operation in command line arguments
 class Task03 {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Write two number for operations");
-        System.out.print("First number: ");
-        int a = sc.nextInt();
-        System.out.print("Second number: ");
-        int b = sc.nextInt();
-        sc.close();
-        System.out.println("The Sum = " +(a +  b));
-        System.out.println("The Subtraction = " +(a - b));
-        System.out.println("The Multiplication = " +(a * b));
-        System.out.println("The Division = " +(a / b));
-        System.out.println("The Modulus = " +(a % b));
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        char c = args[2].charAt(0);
+        switch(c) {
+            case '+':
+            System.out.println("The Sum = " + (num1 + num2));
+            break;
+            case '-':
+            System.out.println("The Subtraction = " + (num1 - num2));
+            break;
+            case '*':
+            System.out.println("The Multiplication = " + (num1 * num2));
+            break;
+            case '/':
+            System.out.println("The Division = " + (num1 / num2));
+            break;
+            case '%':
+            System.out.println("The Modulus = " + (num1 % num2));
+            break;
+            default:
+            System.out.println("wrong argument");
+            break;
+        }
     }
 }
